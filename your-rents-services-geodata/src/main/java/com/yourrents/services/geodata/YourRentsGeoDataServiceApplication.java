2f3a239657fa177b24db4cf5bef9d636761d7cfe
Lucio Benfante <lucio@benfante.com>
2023-10-10 13:07:34 +0200

    Added parent project and initial geodata project

---
+++ your-rents-services-geodata/src/main/java/com/yourrents/services/geodata/YourRentsGeoDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yourrents.services.geodata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourRentsGeoDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourRentsGeoDataServiceApplication.class, args);
+	}
+
+}
---
+++ your-rents-services-geodata/src/test/java/com/yourrents/services/geodata/YourRentsGeoDataServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.yourrents.services.geodata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+@Import(TestYourRentsGeoDataServiceApplication.class)
+class YourRentsGeoDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
