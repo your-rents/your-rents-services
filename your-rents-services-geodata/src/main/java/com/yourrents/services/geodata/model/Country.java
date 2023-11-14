fc4fe47ad91d15afffdb92dfc60b254e82fe26c9
Enrico Giurin <enricogiurin@gmail.com>
2023-11-14 21:46:07 +0100

    issue-19 - implemented the READ operations for Province, Region, Country, Continent - avoiding misunderstanding with inner records

--- your-rents-services-geodata/src/main/java/com/yourrents/services/geodata/model/Country.java
+++ your-rents-services-geodata/src/main/java/com/yourrents/services/geodata/model/Country.java
@@ -3,7 +3,7 @@ package com.yourrents.services.geodata.model;
 import java.util.UUID;
 
 public record Country(UUID uuid, String isoCode, String englishFullName, String iso3, String localName,
-					  Integer number, Continent continent) {
+					  Integer number, Country.Continent continent) {
 
 	public record Continent(UUID uuid, String name) {
 
