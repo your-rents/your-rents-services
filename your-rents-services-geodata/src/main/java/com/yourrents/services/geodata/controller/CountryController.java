d2b5b56181b8182dda009ab7915b85d9c9dcf9d1
Lucio Benfante <lucio@benfante.com>
2023-11-21 15:42:23 +0100

    Added license plugin and headers

--- your-rents-services-geodata/src/main/java/com/yourrents/services/geodata/controller/CountryController.java
+++ your-rents-services-geodata/src/main/java/com/yourrents/services/geodata/controller/CountryController.java
@@ -1,5 +1,25 @@
 package com.yourrents.services.geodata.controller;
 
+/*-
+ * #%L
+ * YourRents GeoData Service
+ * %%
+ * Copyright (C) 2023 Your Rents Team
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.util.UUID;
 
 import org.springdoc.core.annotations.ParameterObject;
